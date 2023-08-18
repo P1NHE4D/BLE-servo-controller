b61563552c354ef8b3255704f94b1786312f91e5
P1NHE4D <aleks.michael.g2@gmail.com>
2023-08-18 15:34:02 +0200

    finish actuator and controller

--- actuator/drivers/src/act_svc.c
+++
@@ -1,5 +0,0 @@
-#include "act_svc/act_svc.h"
-
-BT_GATT_SERVICE_DEFINE(act_svc,
-    BT_GATT_PRIMARY_SERVICE(BT_UUID_ACTS),
-);
--- actuator/src/main.c
+++ actuator/src/main.c
@@ -17,28 +17,37 @@
 #include <zephyr/bluetooth/gatt.h>
 #include <zephyr/sys/byteorder.h>
 
+#include <bluetooth/scan.h>
+#include <bluetooth/gatt_dm.h>
+
 #include <dk_buttons_and_leds.h>
 
 #include "servo/servo.h"
-#include "act_svc/act_svc.h"
 
 LOG_MODULE_REGISTER(Servo_Actuator, LOG_LEVEL_DBG);
 
-#define STACK_SIZE					1024
-#define THREAD_PRIORITY				7
-#define RUN_STATUS_LED				DK_LED1
-#define RUN_LED_BLINK_INTERVAL		500
-#define CONNECTION_STATUS_LED		DK_LED2
-#define DEVICE_NAME 				CONFIG_BT_DEVICE_NAME
-#define DEVICE_NAME_LEN 			(sizeof(DEVICE_NAME) - 1)
+#define STACK_SIZE 1024
+#define THREAD_PRIORITY 7
+#define RUN_STATUS_LED DK_LED1
+#define RUN_LED_BLINK_INTERVAL 500
+#define CONNECTION_STATUS_LED DK_LED2
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
+
+#define BT_UUID_CTRLS_VAL BT_UUID_128_ENCODE(0xD1B81E50, 0x3C0D, 0x11EE, 0x8F23, 0x0800200C9A66)
+#define BT_UUID_CTRLS BT_UUID_DECLARE_128(BT_UUID_CTRLS_VAL)
+#define BT_UUID_CTRLS_ROLL_VAL \
+	BT_UUID_128_ENCODE(0xD1B81E51, 0x3C0D, 0x11EE, 0x8F23, 0x0800200C9A66)
+#define BT_UUID_CTRLS_ROLL BT_UUID_DECLARE_128(BT_UUID_CTRLS_ROLL_VAL)
+
+static K_SEM_DEFINE(servo_angle_lock, 1, 1);
 
 static int blink_status = 0;
-
-static void start_scan(void);
+static int servo_angle = 90;
 
 static struct bt_conn *default_conn;
 
-static struct bt_uuid_16 discover_uuid = BT_UUID_INIT_16(0);
+static struct bt_uuid_128 discover_uuid = BT_UUID_INIT_128(0);
 static struct bt_gatt_discover_params discover_params;
 static struct bt_gatt_subscribe_params subscribe_params;
 
@@ -47,42 +56,54 @@ static uint8_t notify_func(struct bt_conn *conn,
 			   const void *data, uint16_t length)
 {
 	if (!data) {
-		printk("[UNSUBSCRIBED]\n");
+		LOG_INF("[UNSUBSCRIBED]\n");
 		params->value_handle = 0U;
 		return BT_GATT_ITER_STOP;
 	}
 
-	printk("[NOTIFICATION] data %p length %u\n", data, length);
+	int roll = sys_get_le32(((int *)data));
+
+	uint16_t angle;
+	if (roll < 0) {
+		angle = 90 - (roll * -1 / 2);
+	} else {
+		angle = 90 + (roll / 2);
+	}
+	LOG_INF("Angle: %d Roll: %d", angle, roll);
+
+	setAngle(angle);
 
 	return BT_GATT_ITER_CONTINUE;
 }
 
 static uint8_t discover_func(struct bt_conn *conn,
-			     const struct bt_gatt_attr *attr,
-			     struct bt_gatt_discover_params *params)
+							 const struct bt_gatt_attr *attr,
+							 struct bt_gatt_discover_params *params)
 {
 	int err;
 
 	if (!attr) {
-		printk("Discover complete\n");
+		LOG_INF("Discover complete\n");
 		(void)memset(params, 0, sizeof(*params));
 		return BT_GATT_ITER_STOP;
 	}
 
-	printk("[ATTRIBUTE] handle %u\n", attr->handle);
+	LOG_INF("[ATTRIBUTE] handle %u\n", attr->handle);
 
-	if (!bt_uuid_cmp(discover_params.uuid, BT_UUID_HRS)) {
-		memcpy(&discover_uuid, BT_UUID_HRS_MEASUREMENT, sizeof(discover_uuid));
+
+	if (!bt_uuid_cmp(discover_params.uuid, BT_UUID_CTRLS)) {
+		memcpy(&discover_uuid, BT_UUID_CTRLS_ROLL, sizeof(discover_uuid));
 		discover_params.uuid = &discover_uuid.uuid;
 		discover_params.start_handle = attr->handle + 1;
 		discover_params.type = BT_GATT_DISCOVER_CHARACTERISTIC;
 
 		err = bt_gatt_discover(conn, &discover_params);
 		if (err) {
-			printk("Discover failed (err %d)\n", err);
+			LOG_INF("Discover failed (err %d)\n", err);
 		}
+		LOG_INF("Handle: %d", attr->handle);
 	} else if (!bt_uuid_cmp(discover_params.uuid,
-				BT_UUID_HRS_MEASUREMENT)) {
+				BT_UUID_CTRLS_ROLL)) {
 		memcpy(&discover_uuid, BT_UUID_GATT_CCC, sizeof(discover_uuid));
 		discover_params.uuid = &discover_uuid.uuid;
 		discover_params.start_handle = attr->handle + 2;
@@ -90,8 +111,9 @@ static uint8_t discover_func(struct bt_conn *conn,
 		subscribe_params.value_handle = bt_gatt_attr_value_handle(attr);
 
 		err = bt_gatt_discover(conn, &discover_params);
+		LOG_INF("Handle: %d", attr->handle);
 		if (err) {
-			printk("Discover failed (err %d)\n", err);
+			LOG_INF("Discover failed (err %d)\n", err);
 		}
 	} else {
 		subscribe_params.notify = notify_func;
@@ -100,102 +122,16 @@ static uint8_t discover_func(struct bt_conn *conn,
 
 		err = bt_gatt_subscribe(conn, &subscribe_params);
 		if (err && err != -EALREADY) {
-			printk("Subscribe failed (err %d)\n", err);
+			LOG_INF("Subscribe failed (err %d)\n", err);
 		} else {
-			printk("[SUBSCRIBED]\n");
+			LOG_INF("[SUBSCRIBED]\n");
 		}
 
 		return BT_GATT_ITER_STOP;
 	}
+	
 
 	return BT_GATT_ITER_STOP;
-}
-
-static bool eir_found(struct bt_data *data, void *user_data)
-{
-	bt_addr_le_t *addr = user_data;
-	int i;
-
-	LOG_INF("[AD]: %u data_len %u\n", data->type, data->data_len);
-
-	switch (data->type) {
-	case BT_DATA_UUID16_SOME:
-	case BT_DATA_UUID16_ALL:
-		if (data->data_len % sizeof(uint16_t) != 0U) {
-			LOG_ERR("AD malformed\n");
-			return true;
-		}
-
-		for (i = 0; i < data->data_len; i += sizeof(uint16_t)) {
-			struct bt_le_conn_param *param;
-			struct bt_uuid *uuid;
-			uint16_t u16;
-			int err;
-
-			// TODO: CHANGE
-			memcpy(&u16, &data->data[i], sizeof(u16));
-			uuid = BT_UUID_DECLARE_16(sys_le16_to_cpu(u16));
-			if (bt_uuid_cmp(uuid, BT_UUID_HRS)) {
-				continue;
-			}
-
-			err = bt_le_scan_stop();
-			if (err) {
-				LOG_ERR("Stop LE scan failed (err %d)\n", err);
-				continue;
-			}
-
-			param = BT_LE_CONN_PARAM_DEFAULT;
-			err = bt_conn_le_create(addr, BT_CONN_LE_CREATE_CONN,
-						param, &default_conn);
-			if (err) {
-				LOG_ERR("Create conn failed (err %d)\n", err);
-				start_scan();
-			}
-
-			return false;
-		}
-	}
-
-	return true;
-}
-
-static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
-			 struct net_buf_simple *ad)
-{
-	char dev[BT_ADDR_LE_STR_LEN];
-
-	bt_addr_le_to_str(addr, dev, sizeof(dev));
-	LOG_INF("[DEVICE]: %s, AD evt type %u, AD data len %u, RSSI %i\n",
-	       dev, type, ad->len, rssi);
-
-	/* We're only interested in connectable events */
-	if (type == BT_GAP_ADV_TYPE_ADV_IND ||
-	    type == BT_GAP_ADV_TYPE_ADV_DIRECT_IND) {
-		bt_data_parse(ad, eir_found, (void *)addr);
-	}
-}
-
-static void start_scan(void)
-{
-	int err;
-
-	/* Use active scanning and disable duplicate filtering to handle any
-	 * devices that might update their advertising data at runtime. */
-	struct bt_le_scan_param scan_param = {
-		.type       = BT_LE_SCAN_TYPE_ACTIVE,
-		.options    = BT_LE_SCAN_OPT_NONE,
-		.interval   = BT_GAP_SCAN_FAST_INTERVAL,
-		.window     = BT_GAP_SCAN_FAST_WINDOW,
-	};
-
-	err = bt_le_scan_start(&scan_param, device_found);
-	if (err) {
-		LOG_ERR("Scanning failed to start (err %d)\n", err);
-		return;
-	}
-
-	LOG_INF("Scanning successfully started\n");
 }
 
 static void connected(struct bt_conn *conn, uint8_t conn_err)
@@ -205,57 +141,129 @@ static void connected(struct bt_conn *conn, uint8_t conn_err)
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
-	if (conn_err) {
-		printk("Failed to connect to %s (%u)\n", addr, conn_err);
+	if (conn_err)
+	{
+		LOG_INF("Failed to connect to %s (%d)", addr, conn_err);
 
-		bt_conn_unref(default_conn);
-		default_conn = NULL;
+		if (default_conn == conn)
+		{
+			bt_conn_unref(default_conn);
+			default_conn = NULL;
 
-		start_scan();
+			err = bt_scan_start(BT_SCAN_TYPE_SCAN_ACTIVE);
+			if (err)
+			{
+				LOG_ERR("Scanning failed to start (err %d)",
+						err);
+			}
+		}
+
 		return;
 	}
 
-	printk("Connected: %s\n", addr);
+	LOG_INF("Connected: %s", addr);
 
-	if (conn == default_conn) {
-		memcpy(&discover_uuid, BT_UUID_HRS, sizeof(discover_uuid));
-		discover_params.uuid = &discover_uuid.uuid;
-		discover_params.func = discover_func;
-		discover_params.start_handle = BT_ATT_FIRST_ATTRIBUTE_HANDLE;
-		discover_params.end_handle = BT_ATT_LAST_ATTRIBUTE_HANDLE;
-		discover_params.type = BT_GATT_DISCOVER_PRIMARY;
+	memcpy(&discover_uuid, BT_UUID_CTRLS, sizeof(discover_uuid));
+	discover_params.uuid = &discover_uuid.uuid;
+	discover_params.func = discover_func;
+	discover_params.start_handle = BT_ATT_FIRST_ATTRIBUTE_HANDLE;
+	discover_params.end_handle = BT_ATT_LAST_ATTRIBUTE_HANDLE;
+	discover_params.type = BT_GATT_DISCOVER_PRIMARY;
 
-		err = bt_gatt_discover(default_conn, &discover_params);
-		if (err) {
-			printk("Discover failed(err %d)\n", err);
-			return;
-		}
+	err = bt_gatt_discover(default_conn, &discover_params);
+	if (err) {
+		LOG_ERR("Discover failed(err %d)\n", err);
+		return;
+	}
+
+	err = bt_scan_stop();
+	if ((!err) && (err != -EALREADY))
+	{
+		LOG_ERR("Stop LE scan failed (err %d)", err);
 	}
 }
 
 static void disconnected(struct bt_conn *conn, uint8_t reason)
 {
 	char addr[BT_ADDR_LE_STR_LEN];
+	int err;
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
-	printk("Disconnected: %s (reason 0x%02x)\n", addr, reason);
+	LOG_INF("Disconnected: %s (reason %u)", addr, reason);
 
-	if (default_conn != conn) {
+	if (default_conn != conn)
+	{
 		return;
 	}
 
 	bt_conn_unref(default_conn);
 	default_conn = NULL;
 
-	start_scan();
+	err = bt_scan_start(BT_SCAN_TYPE_SCAN_ACTIVE);
+	if (err)
+	{
+		LOG_ERR("Scanning failed to start (err %d)",err);
+	}
 }
-
 
 BT_CONN_CB_DEFINE(conn_callbacks) = {
 	.connected = connected,
-	.disconnected = disconnected,
-};
+	.disconnected = disconnected};
+
+static void scan_filter_match(struct bt_scan_device_info *device_info,
+							  struct bt_scan_filter_match *filter_match,
+							  bool connectable)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(device_info->recv_info->addr, addr, sizeof(addr));
+
+	LOG_INF("Filters matched. Address: %s connectable: %d",
+			addr, connectable);
+}
+
+static void scan_connecting_error(struct bt_scan_device_info *device_info)
+{
+	LOG_WRN("Connecting failed");
+}
+
+static void scan_connecting(struct bt_scan_device_info *device_info,
+							struct bt_conn *conn)
+{
+	LOG_INF("Connecting...");
+	default_conn = bt_conn_ref(conn);
+}
+
+BT_SCAN_CB_INIT(scan_cb, scan_filter_match, NULL, scan_connecting_error, scan_connecting);
+
+static int scan_init(void)
+{
+	int err;
+	struct bt_scan_init_param scan_init = {
+		.connect_if_match = 1,
+	};
+
+	bt_scan_init(&scan_init);
+	bt_scan_cb_register(&scan_cb);
+
+	err = bt_scan_filter_add(BT_SCAN_FILTER_TYPE_UUID, BT_UUID_CTRLS);
+	if (err)
+	{
+		LOG_ERR("Scanning filters cannot be set (err %d)", err);
+		return err;
+	}
+
+	err = bt_scan_filter_enable(BT_SCAN_UUID_FILTER, false);
+	if (err)
+	{
+		LOG_ERR("Filters cannot be turned on (err %d)", err);
+		return err;
+	}
+
+	LOG_INF("Scan module initialized");
+	return err;
+}
 
 int main(void)
 {
@@ -267,6 +275,8 @@ int main(void)
 		return -1;
 	}
 
+	setAngle(servo_angle);
+
 	err = bt_enable(NULL);
 	if (err)
 	{
@@ -275,7 +285,22 @@ int main(void)
 	}
 	LOG_INF("Bluetooth initialized\n");
 
-	start_scan();
+	err = scan_init();
+	if (err != 0)
+	{
+		LOG_ERR("scan_init failed (err %d)", err);
+		return 0;
+	}
+
+	err = bt_scan_start(BT_SCAN_TYPE_SCAN_ACTIVE);
+	if (err)
+	{
+		LOG_ERR("Scanning failed to start (err %d)", err);
+		return 0;
+	}
+
+	LOG_INF("Scanning successfully started");
+
 
 	while (true)
 	{
@@ -285,10 +310,3 @@ int main(void)
 
 	return 0;
 }
-
-void update_servo_thread(void) {
-	// retrieve value from controller
-	// set servo angle accordingly
-	// sleep 
-}
-
